990d5df95d02bfc99d95c35580617cf48deed04d
Carl Larson <cslarson@gmail.com>
2021-03-11 19:56:19 -0500

    round down if donuts > 0

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,10 @@
 const corsProxyUrl = 'https://cors.bridged.cc'
 
-export const commaNumber = num => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").replace(/[^0-9^,]+/g, '');
+export const commaNumber = num => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").replace(/[^0-9^,^\.]+/g, '');
 
 export const shortNum = donuts => {
   let val = NaN;
+  if (donuts > 0) donuts = Math.floor(donuts)
   if (donuts < 100000) val = commaNumber(donuts);
   else if (donuts < 1000000) val = Math.floor(donuts/1000) + 'K';
   else val = Math.floor(donuts/100000)/10 + 'M';
